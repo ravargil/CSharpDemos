7f7d61df9a81da7e0f539f8fea8ddd886e231b12
Raanan Avargil <ravargil@gmail.com>
2013-12-30 21:16:32 +0200

    Add SearchInFile project

---
+++ SearchingInFile/SearchingInFile/FileSearcher.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SearchingInFile
+{
+    public class FileSearcher
+    {
+        public FileSearcher(string filePath)
+	    {
+            this._filePath = filePath;
+            this.ErrorMessage = "";
+            this._nameToIds = new Dictionary<string, List<int>>();
+            this._emailToIds = new Dictionary<string, List<int>>();
+	    }
+
+        public string ErrorMessage { get; private set; }
+
+        public List<int> Search(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return new List<int>();
+
+            if (isEmail(str))
+                return search(str, this._emailToIds);
+
+            return search(str, this._nameToIds);
+        }
+
+        public bool Init()
+        {
+            try
+            {
+                using (var sr = new StreamReader(_filePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var splits = sr.ReadLine().Split(',');
+                        int id = int.Parse(splits[0].Trim());
+                        string name = splits[1].Trim();
+                        string email = splits[2].Trim();
+
+                        add(name, id, _nameToIds);
+                        add(email, id, _emailToIds);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorMessage = "File cannot be opened";
+                return false;                
+            }
+
+            return true;
+            
+        }
+
+
+        #region Private Methods 
+
+        private List<int> search(string str, Dictionary<string, List<int>> container)
+        {
+            List<int> result;
+            if (container.TryGetValue(str, out result))
+                return result;
+
+            return new List<int>();
+        }
+
+        private bool isEmail(string str)
+        {
+            return str.Contains("@");
+        }
+
+        private void add(string str, int id, Dictionary<string, List<int>> container)
+        {
+            List<int> ids;
+            if (container.TryGetValue(str, out ids))
+            {
+                //if the given string (name or email) is already in the container, 
+                //just add the new id to its list:
+                ids.Add(id);
+            }
+            else
+            {
+                //Otherwise, create a new list with the given id:
+                container.Add(str, new List<int>() { id });
+            }
+        }
+
+        #endregion
+
+        ///
+        /// Private Fields
+        ///
+        private string _filePath;
+        private Dictionary<string, List<int>> _nameToIds;
+        private Dictionary<string, List<int>> _emailToIds;
+    }
+}
---
+++ SearchingInFile/SearchingInFileTests/FileSearcherTests.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using SearchingInFile;
+
+namespace SearchingInFileTests
+{
+    [TestClass]
+    public class FileSearcherTests
+    {
+        [TestMethod]
+        [Description("Test Init method when file is not exits. Expected result: false and error message")]
+        public void TestInit_NoSuchFile_ReturnFalse()
+        {
+            //Arrange:
+            FileSearcher fileSearcher = new FileSearcher("NoSuchFile");
+
+            //Act:
+            bool result = fileSearcher.Init();
+
+            //Assert:
+            Assert.IsFalse(result);
+            Assert.AreEqual(fileSearcher.ErrorMessage, "File cannot be opened");
+        }
+
+        [TestMethod]
+        [Description("Test Init method when file exits. Expected results: true and empty error message")]
+        public void TestInit_FileExits_ReturnTrue()
+        {
+            //Arrange:
+            string filePath = @"..\..\EmptyFile.csv";
+            FileSearcher fileSearcher = new FileSearcher(filePath);
+
+            //Act:
+            bool result = fileSearcher.Init();
+
+            //Assert:
+            Assert.IsTrue(result);
+            Assert.IsTrue(string.IsNullOrEmpty(fileSearcher.ErrorMessage));
+        }
+
+
+        [TestMethod]
+        [Description("Test Search method when there is no such email. Expected result: empty list")]
+        public void TestSearch_NoSuchEmail_ReturnEmptyList()
+        {
+            //Arrange:
+            string filePath = @"..\..\EmptyFile.csv";
+            FileSearcher fileSearcher = new FileSearcher(filePath);
+            bool initResult = fileSearcher.Init();
+
+            //Act:
+            List<int> result = fileSearcher.Search("no@such.email");
+            
+            //Assert:
+            Assert.AreEqual(result.Count, 0);
+        }
+
+        [TestMethod]
+        [Description("Test Search method when there is no such name. Expected result: empty list")]
+        public void TestSearch_NoSuchName_ReturnEmptyList()
+        {
+            //Arrange:
+            string filePath = @"..\..\EmptyFile.csv";
+            FileSearcher fileSearcher = new FileSearcher(filePath);
+            bool initResult = fileSearcher.Init();
+
+            //Act:
+            List<int> result = fileSearcher.Search("No Such Name");
+
+            //Assert:
+            Assert.AreEqual(result.Count, 0);
+        }
+
+        [TestMethod]
+        [Description("Test Search in file when given a null string. Expected result: empty list")]
+        public void TestSearch_NullString_ReturnEmptyList()
+        {
+            //Arrange:
+            FileSearcher fileSearcher = new FileSearcher("NoSuchFile");
+
+            //Act:
+            List<int> result = fileSearcher.Search(null);
+
+            //Assert:
+            Assert.AreEqual(result.Count, 0);
+        }
+
+        [TestMethod]
+        [Description("Test Search in file when given an empty string. Expected result: empty list")]
+        public void TestSearch_EmptyString_ReturnEmptyList()
+        {
+            //Arrange:
+            FileSearcher fileSearcher = new FileSearcher("NoSuchFile");
+
+            //Act:
+            List<int> result = fileSearcher.Search("");
+
+            //Assert:
+            Assert.AreEqual(result.Count, 0);
+        }
+
+        [TestMethod]
+        [Description("Test Search method when there is only one line for the email. Expected result: list with one id")]
+        public void TestSearch_SingleEmailOccurance_ReturnListWithOneId()
+        {
+            //Arrange:
+            string filePath = @"..\..\SearchInFile.csv";
+            FileSearcher fileSearcher = new FileSearcher(filePath);
+            bool initResult = fileSearcher.Init();
+
+            //Act:
+            List<int> result = fileSearcher.Search("Denis@Dentler.com");
+
+            //Assert:
+            Assert.AreEqual(result.Count, 1);
+            Assert.AreEqual(result[0], 25);
+        }
+
+        [TestMethod]
+        [Description("Test Search method when there are multiple lines for the same email. Expected result: list with three items")]
+        public void TestSearch_MultipleEmailOccurances_ReturnListWithThreeId()
+        {
+            //Arrange:
+            string filePath = @"..\..\SearchInFile.csv";
+            FileSearcher fileSearcher = new FileSearcher(filePath);
+            bool initResult = fileSearcher.Init();
+
+            //Act:
+            List<int> result = fileSearcher.Search("Hazel@Zemke.com");
+
+            //Assert:
+            Assert.AreEqual(result.Count, 3);
+        }
+
+
+        [TestMethod]
+        [Description("Test Search method when there is only one line for the name. Expected result: list with one id")]
+        public void TestSearch_SingleNameOccurance_ReturnListWithOneId()
+        {
+            //Arrange:
+            string filePath = @"..\..\SearchInFile.csv";
+            FileSearcher fileSearcher = new FileSearcher(filePath);
+            bool initResult = fileSearcher.Init();
+
+            //Act:
+            List<int> result = fileSearcher.Search("Denis Dentler");
+
+            //Assert:
+            Assert.AreEqual(result.Count, 1);
+            Assert.AreEqual(result[0], 25);
+        }
+
+        [TestMethod]
+        [Description("Test Search method when there are multiple lines for the same name. Expected result: list with three items")]
+        public void TestSearch_MultipleNameOccurances_ReturnListWithThreeId()
+        {
+            //Arrange:
+            string filePath = @"..\..\SearchInFile.csv";
+            FileSearcher fileSearcher = new FileSearcher(filePath);
+            bool initResult = fileSearcher.Init();
+
+            //Act:
+            List<int> result = fileSearcher.Search("Jane Cockrum");
+
+            //Assert:
+            Assert.AreEqual(result.Count, 4);
+        }
+
+    }
+}
+
+
